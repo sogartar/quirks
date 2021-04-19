12f12ac89767342158b453495445aadb02878459
Sogartar <sogartary@yahoo.com>
2021-04-18 21:29:35 -0700

    Bump version to 0.1.1

--- make.bat
+++ make.bat
@@ -7,7 +7,7 @@ mkdir "%SCRIPT_DIR%\brushes"
 bbrusher pack "%SCRIPT_DIR%\brushes\quirks_ui.brush" "%SCRIPT_DIR%\gfx" || goto error
 
 cd "%SCRIPT_DIR%"
-7z a "%WORKING_DIR%\mod_quirks0.1.zip" brushes gfx scripts sounds || goto error
+7z a "%WORKING_DIR%\mod_quirks0.1.1.zip" brushes gfx scripts sounds || goto error
 exit 0
 
 :error
--- scripts/!mods_preload/quirks.nut
+++ scripts/!mods_preload/quirks.nut
@@ -1,4 +1,4 @@
-::mods_registerMod("quirks", 0.1, "quirks");
+::mods_registerMod("quirks", 0.1.1, "quirks");
 
 local gt = this.getroottable();
 
