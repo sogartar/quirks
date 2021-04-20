099a6db72505e0d282a35e04a633e91e14d24eea
Sogartar <sogartary@yahoo.com>
2021-04-19 20:11:50 -0700

    Bump debug version to 0.1.3

--- make.bat
+++ make.bat
@@ -7,7 +7,7 @@ mkdir "%SCRIPT_DIR%\brushes"
 bbrusher pack "%SCRIPT_DIR%\brushes\quirks_ui.brush" "%SCRIPT_DIR%\gfx" || goto error
 
 cd "%SCRIPT_DIR%"
-7z a "%WORKING_DIR%\mod_quirks0.1.2.zip" brushes gfx scripts sounds || goto error
+7z a "%WORKING_DIR%\mod_quirks0.1.3.zip" brushes gfx scripts sounds || goto error
 exit 0
 
 :error
